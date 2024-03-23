0becc1dc13b746d6d7eb3e5d8277e6900a2e7261
sauce__77 <sauceortediego505@gmail.com>
2024-03-22 18:15:36 -0600

    Se agrego la funcion para obtener ruta de archivo

--- NRFM_Auditoria/NRFM_Auditoria/Form1.cs
+++ NRFM_Auditoria/NRFM_Auditoria/Form1.cs
@@ -4,14 +4,32 @@ namespace NRFM_Auditoria
 {
     public partial class Form1 : Form
     {
+
+        // filtro utilizado para solo mostrar acrhivos de excel
+        public const string FILTROS_ARCHIVOS_EXCEL = "Archivos Excel|*.xlsx;*.xlsm;*.csv;";
         public Form1()
         {
             InitializeComponent();
         }
 
+        public string obtenerArchivoSeleccionado()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = FILTROS_ARCHIVOS_EXCEL;
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }// si el archivo esta ok
+
+            return null;
+        }
         private void cargarArchivo_Click(object sender, EventArgs e)
         {
             Debug.WriteLine("Hola soy el Anticristo666");
+            string archivo = obtenerArchivoSeleccionado();
+            Debug.WriteLine(archivo);
         }
     }
 }
